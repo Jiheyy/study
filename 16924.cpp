a210ab7aef4608204a688b34a0149c214d3ba836
min <jihye.min.2016@gmail.com>
2022-01-20 01:52:26 +0900

    mod

--- 16924.cpp
+++ 16924.cpp
@@ -10,38 +10,33 @@ int x, y, s;
 };
 
 int check(int y, int x) {
-	int i=1;
+	int i=0;
 	while(true) {
+		i++;
 		if(map[y-i][x] == '*' && map[y+i][x] == '*' 
 			&& map[y][x+i] == '*' && map[y][x-i] == '*') {
 			visit[y-i][x] = true;
 			visit[y+i][x] = true;
 			visit[y][x+i] = true;
 			visit[y][x-i] = true;
-			
-			i++;
 		}
 
 		else {
+			i--;
 			break;
 		}
 	}
-	i--;
-	if(i > 0)
-		visit[y][x] = true;
-	/*
-	for(int i=0; i<n; i++) {
-		for(int j=0; j<m; j++) {
-			if(visit[i][j] == true)
-				printf("*");
-			else
-				printf(".");
-		}
-		printf("\n");
-	}
-	*/
 
-	return i*4+1;
+	if(i != 0) {
+		for(int j=0; j<i; j++) {
+			visit[y-j][x] = true;
+			visit[y+j][x] = true;
+			visit[y][x-j] = true;
+			visit[y][x+j] = true;
+		}
+		return i; 
+	}
+	return 0;
 }
 
 int main() {
@@ -56,7 +51,7 @@ int main() {
 		for(int j=1; j<m-1; j++){
 			if(map[i][j-1] == '*' && map[i][j] == '*'&& map[i][j+1] == '*'){
 				int sizes = check(i, j);
-				if(sizes >= 5){
+				if(sizes > 0){
 					A a;
 					a.y = i+1;
 					a.x = j+1;
@@ -67,22 +62,18 @@ int main() {
 		}
 	}
 
-	int cnt = 0;
-	int star = 0;
 	for(int i=0; i<n; i++)
 		for(int j=0; j<m; j++){
-			if(visit[i][j]== true)
-				cnt++;
-			if(map[i][j] == '*')
-				star++;
+			if((visit[i][j] == true && map[i][j] == '.') ||(visit[i][j] == false && map[i][j] == '*')) {
+				printf("-1\n");
+				exit(0);
+			}
 		}
 
 
-	if(cnt == star && cnt != 0) {
-		printf("%lu\n", v.size());
 
-	}
-	else 
-		printf("-1\n");
+	printf("%lu\n", v.size());
+	for(int i=0; i<v.size(); i++)
+		printf("%d %d %d\n", v[i].y, v[i].x, v[i].s);
 	return 0;
 }
